4709e28fd9b2069e1fa9e324fbec3d8e7685a4ff
gebeen-dev <rrdd1207@gmail.com>
2024-09-24 09:37:03 +0900

    add Checkoutcnt 클래스 생성

---
+++ src/numplay/Checkoutcnt.java
@@ -0,0 +1,13 @@
+package numplay;
+
+import java.util.List;
+
+public class Checkoutcnt {
+    public void checkoutcnt(int outCnt, List<Integer> numbers, List<Integer> answers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (!answers.contains(numbers.get(i))) {
+                outCnt++;
+            }
+        }
+    }
+}
