0e601d2d783db4f122cc49956dd655607f1c5881
gebeen-dev <rrdd1207@gmail.com>
2024-09-24 12:08:26 +0900

    update 게임 반복 기능 추가

--- src/numplay/BaseballGame.java
+++ src/numplay/BaseballGame.java
@@ -7,23 +7,21 @@ import static java.lang.Character.isDigit;
 
 public class BaseballGame {
     public static void main(String[] args) {
-        System.out.println("< 게임을 시작합니다 >");
         Scanner scanner = new Scanner(System.in);
 
-        //ball, out, strike 인스턴스 생성
+        //ball, out, strike 인스턴스
         Checkballcnt checkballcnt = new Checkballcnt();
         Checkstrikecnt checkstrikecnt = new Checkstrikecnt();
         Checkoutcnt checkoutcnt = new Checkoutcnt();
         RendomNum rendomNum = new RendomNum();
 
-        //제네릭(<>)에선 무조건 클래스타임을 써야한다
-        List<Integer> answers = rendomNum.create();
-        //사용자에게 받을 값
-        List<Integer> numbers = new ArrayList<>();
-
-        int strikeCnt, ballCnt, outCnt;
-
         while (true) {
+            System.out.println("< 게임을 시작합니다 >");
+
+            List<Integer> answers = rendomNum.create();  // 새로운 정답 생성
+            List<Integer> numbers = new ArrayList<>();
+            int strikeCnt, ballCnt, outCnt;
+
             try {
                 //게임 초기화
                 strikeCnt = 0;
@@ -43,7 +41,7 @@ public class BaseballGame {
                         // throw : 예외를 던지다/ 에외 처리 학습
                         // 예외처리 이후 게임 재시작 할 수 없음 -> try catch 사용
                         throw new RuntimeException("숫자만 입력 해 주세요");
-                    } else if (isDigit(ch) && ch =='0') {
+                    } else if (ch =='0') {
                         throw new RuntimeException("1~9까지 숫자를 사용해 주세요");
                     }
 
